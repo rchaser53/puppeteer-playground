7c38d3e86726bf61e18249bf27ec2db48d26ac72
rchaser53 <tayoshizawa29@gmail.com>
2018-05-07 22:15:05 +0900

    take the picture over all

--- index.js
+++ index.js
@@ -7,17 +7,27 @@ const getImages = async (src, index) => {
 }
 
 const getAllJpegInPage = async (url) => {
+  try {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   let _ = await page.goto(url);
 
-  const texts = await page.$eval('#last_last', e => {
+  const texts = await page.$eval('#last_last', (e) => {
     return e.innerText
   });
 
   console.log(texts)
 
-  browser.close();  
+  await page.screenshot({
+    path: './img/overAll.jpg',
+    quality: 30,
+    fullPage: true
+  });
+
+  browser.close();
+  } catch (err) {
+    console.error(err)
+  }
 };
 
 getAllJpegInPage('https://jp.investing.com/currencies/btc-usd');
